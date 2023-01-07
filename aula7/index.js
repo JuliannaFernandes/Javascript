786a4c9a229a34d346bdc101b89480587cdf01f7
Julianna Fernandes <juliannabf29@gmail.com>
2023-01-07 19:24:20 -0300

    Aula 7 - Constantes com const

---
+++ aula7/index.js
@@ -0,0 +1,11 @@
+// String = Texto | Número = Número
+
+const primeiroNumero = 6;
+
+const segundoNumero = 9;
+
+const resultado = primeiroNumero * segundoNumero;
+
+const resultadoDuplicado = resultado * 2;
+
+console.log("O resultado é:", resultadoDuplicado);
