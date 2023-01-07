33e8d8058403dd4849f78186cc0fc928597aa6ac
Julianna Fernandes <juliannabf29@gmail.com>
2023-01-07 20:14:54 -0300

    Aula 9 - Tipos de dados

---
+++ aula9/index.js
@@ -0,0 +1,17 @@
+// String, number, undefined, null, boolean
+const nome = 'Julianna'; // string
+const nome1 = 'Julianna'; // string
+const nome2 = 'Julianna'; // string
+const num1 = 10; // number
+const num2 = 10.52; // number
+let nomeAluno; // undefined -> não aponta pra local nenhuma na memória
+const sobrenomeAluno = null; // Nulo -> não aponta pra local nenhuma na memória
+const aprovado = false; // Boolean = true, false (lógico)
+
+
+let a = 2;
+const b = a;
+console.log(a, b); 
+
+a = 3;
+console.log(a, b); 
