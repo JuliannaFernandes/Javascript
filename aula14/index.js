82b6f950a2f1fd4bac35ab157bc7af39c6788677
Julianna Fernandes <juliannabf29@gmail.com>
2023-01-12 20:49:51 -0300

    Aula 14 - Objeto Math

---
+++ aula14/index.js
@@ -0,0 +1,11 @@
+let numero1 = 9.5586;
+//let numero2 = Math.floor(numero1); // para arredondar para baixo
+//let numero2 = Math.ceil(numero1); // para arredondar para cima
+//let numero2 = Math.round(numero1); // para arredondar para o mais próximo  
+//console.log(numero2);
+//console.log (Math.max(1,5,27639,3,6,9,10,11)); // para exibir o número maior 
+//console.log (Math.min(1,5,27639,3,6,9,10,11)); // para exibir o número menor 
+const numeroAleatorio = Math.random (Math.random() * (10-5) + 5);
+console.log(numeroAleatorio); // para exibir o número aleatório
+
+
