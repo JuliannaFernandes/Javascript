1043ed1045b5f9e707c28b12706ca8ff461e61b4
Julianna Fernandes <juliannabf29@gmail.com>
2023-04-01 19:07:32 -0300

    aula16 - Array (básico)

---
+++ aula16/index.js
@@ -0,0 +1,35 @@
+//criando array básico
+// indexado         0         1         2
+const alunos = ["Julianna", "João", "Felipe"];
+
+// console.log(typeof alunos); // tipo de variavel
+// console.log(alunos instanceof Array); // pergunta se é um array
+
+// console.log(alunos.slice(0, 3)); // mostrar até
+
+//console.log(alunos.slice(0, -1)); // mostrar até
+
+// delete alunos[1]; // Remover e não alterar índice.
+// console.log(alunos);
+
+//alunos.pop(); // Remove do fim
+//const indiceRemovido = alunos.pop ();
+// console.log(indiceRemovido);
+
+// alunos.shift(); //Remove do início
+//const indiceRemovido = alunos.shift ();
+// console.log(indiceRemovido);
+
+//alunos.unshift("Lalau"); // Adiciona no começo
+//alunos.unshift("Aninha");
+
+//alunos.push("Laiza"); // Adiciona no fim
+
+//alunos[alunos.length] = "Luiza"; //Adiciona no fim
+
+//alunos[3] = "Gabriella"; //Adiciona um índice
+
+//console.log(alunos[0]); // Exibir índice do array
+//console.log(alunos[2]);
+
+//alunos[0] = "Rosana"; // Alterar valor
