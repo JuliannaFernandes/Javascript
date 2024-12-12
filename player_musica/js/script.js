60985b526a055e73f99a08a38ff91876c40ea072
Julianna Fernandes <juliannabf29@gmail.com>
2024-12-12 18:27:26 -0300

    feat: criando function para atualizar a barra de acordo com a música

--- player_musica/js/script.js
+++ player_musica/js/script.js
@@ -2,7 +2,9 @@ let musica = document.querySelector('audio'); // pegando tag audio
 
 document.querySelector('.btn-play').addEventListener('click', tocarMusica); // adicionando evento de click no botão play
 
-document.querySelector('.btn-pause').addEventListener('click', pausarMusica);  
+document.querySelector('.btn-pause').addEventListener('click', pausarMusica);
+
+musica.addEventListener('timeupdate', atualizarBarra)
 
 function tocarMusica() {
     musica.play();
@@ -14,4 +16,11 @@ function pausarMusica() {
     musica.pause();
     document.querySelector('.btn-play').style.display = 'block';
     document.querySelector('.btn-pause').style.display = 'none';
+}
+
+function atualizarBarra() {
+    let barra = document.querySelector('progress')
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
+    let tempoDecorrido = document.querySelector('.inicioMusica');
+    tempoDecorrido.textContent = Math.floor(musica.currentTime);
 }
