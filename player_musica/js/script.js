ad013d099ea3134791c607e8678c2bdcc433188e
Julianna Fernandes <juliannabf29@gmail.com>
2024-12-12 17:59:01 -0300

    feat: criando function para pausar música

--- player_musica/js/script.js
+++ player_musica/js/script.js
@@ -2,9 +2,16 @@ let musica = document.querySelector('audio'); // pegando tag audio
 
 document.querySelector('.btn-play').addEventListener('click', tocarMusica); // adicionando evento de click no botão play
 
+document.querySelector('.btn-pause').addEventListener('click', pausarMusica);  
+
 function tocarMusica() {
     musica.play();
     document.querySelector('.btn-pause').style.display = 'block';
     document.querySelector('.btn-play').style.display = 'none';
+}
 
+function pausarMusica() {
+    musica.pause();
+    document.querySelector('.btn-play').style.display = 'block';
+    document.querySelector('.btn-pause').style.display = 'none';
 }
